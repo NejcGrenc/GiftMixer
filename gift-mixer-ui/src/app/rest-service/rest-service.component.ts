ec743f05876c4e7efa7a602c4f188fe3a1e158ac
Nejc <Nejc@DESKTOP-UOHP1VN>
2019-12-01 02:14:44 +0100

    configure angular prod environment

--- gift-mixer-ui/src/app/rest-service/rest-service.component.ts
+++ gift-mixer-ui/src/app/rest-service/rest-service.component.ts
@@ -1,21 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import {environment} from '../../environments/environment';
 
 @Injectable()
 export class RestServiceComponent {
 
   constructor(private http: HttpClient) { }
-
-  baseUrl = 'http://localhost:9000';
-//  baseUrl = 'http://grenc.eu:9000';
   
   send(path: string, data: any): Observable<boolean> {
-    return this.http.post<boolean>(this.baseUrl + path, data);
+    return this.http.post<boolean>(environment.baseUrl + path, data);
   }
   
   fetch<T>(path: string, data: any): Observable<RestResponse<T>> {
-    return this.http.post<RestResponse<T>>(this.baseUrl + path, data);
+    return this.http.post<RestResponse<T>>(environment.baseUrl + path, data);
   }
 }
 
